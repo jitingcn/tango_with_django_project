daced5e621a54f795bbac7507ad49a398ea80c16
jitingcn <jiting@jtcat.com>
2020-01-27 17:30:21 +0000

    Completed Chapter 4
    Chapter 4.2
    Chapter 4.3
    Remove nested folders

--- rango/views.py
+++ rango/views.py
@@ -3,8 +3,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
+# def index(request):
+#     return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
 def index(request):
-#    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage matches to {{ boldmessage }} in the template!
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
@@ -15,6 +16,8 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 
+# def about(request):
+#     return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
-
+    context_dict = {'boldmessage': 'This tutorial has been put together by jitingcn.' }
+    return render(request, 'rango/about.html', context=context_dict)
