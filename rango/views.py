7464a3d0e127cad672bfd213595f3d9c101423eb
jitingcn <jiting@jtcat.com>
2020-02-03 03:11:11 +0000

    [TEST] Completed Chapter 9 exercises

--- rango/views.py
+++ rango/views.py
@@ -67,6 +67,7 @@ def show_category(request, category_name_slug):
     return render(request, 'rango/category.html', context=context_dict)
 
 
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -92,6 +93,7 @@ def add_category(request):
     return render(request, 'rango/add_category.html', {'form': form})
 
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -181,7 +183,7 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html')
 
 
 @login_required
